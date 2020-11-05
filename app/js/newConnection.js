e51bfee5a986901f31e22d3930f49f0a0ca3e236
Prithvipal Singh <prithvirathore99@gmail.com>
2020-11-05 09:14:34 +0530

    Test connection with logs

--- app/js/newConnection.js
+++ app/js/newConnection.js
@@ -1,36 +1,54 @@
+const util = require('util')
 const {ipcRenderer} = require('electron')
 const MongoClient = require('mongodb').MongoClient;
-testConn = document.getElementById("btn_test_conn")
+
+let testConn = document.getElementById("btn_test_conn")
 // Connection URL
-const url = 'mongodb://localhost:27016';
+// const url =  'mongodb://localhost:27016';
+
 
 testConn.addEventListener("click", e =>{
-     e.preventDefault()
-    auth = getAuthDetails()
-
+    e.preventDefault()
+    const auth = getAuthDetails()
+    const url = getConnURL(auth)
+    console.log("Connecting to:::", url)
     MongoClient.connect(url, {useUnifiedTopology: true,useNewUrlParser:true, poolSize: 5, reconnectInterval: 500 },
-        function(err,client){
+        function(err, client){
+            if (err){
+                console.log("EEEEEerr", err)
+            }else {
+                console.log("Successful")
+            }
+
             //console.log("DATABASE IS BEING LOGGED...." ,client);
-            var dbAdmin=client.db("test").admin();
-        dbAdmin.listDatabases(function (err,databases) {
-            console.log("before adding databases");
-            console.log(databases);
-            client.close();
-        })
+            // var dbAdmin=client.db("test").admin();
+            // dbAdmin.listDatabases(function (err,databases) {
+            //     console.log("before adding databases");
+            //     console.log(databases);
+            //     client.close();
+            // })
         });
     
    // ipcRenderer.send("connection:test", auth)
 })
+
 function getAuthDetails(){
-    hostname = document.getElementById('hostname').value
-    port = document.getElementById('port').value
-    username = document.getElementById('username').value
-    password = document.getElementById('password').value
-    auth = {
+    const hostname = document.getElementById('hostname').value
+    const port = document.getElementById('port').value
+    const username = document.getElementById('username').value
+    const password = document.getElementById('password').value
+    const auth = {
         hostname,
         port,
         username,
         password
     }
     return auth
+}
+
+function getConnURL(auth){
+    if(auth.port === ''){
+        const url =  util.format('mongodb+srv://%s:%s@%s', auth.username, auth.password, auth.hostname);
+        return url
+    }
 }
